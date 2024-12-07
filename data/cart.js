cb8630a162c4c54e836a1e83b31ef4cd917eb9ed
flowihs <zmulenko500@gmail.com>
2024-12-07 21:45:51 +0300

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,12 @@
-const cart = [
+let cart = [
     {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+
+    },
+
+    {
+        productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
         quantity: 2,
 
     }
@@ -27,4 +33,16 @@ function addToCart (productId) {
     }
   }
 
-  export {addToCart, cart}
+  function removeFromCart (productId) {
+    const newCart = []
+
+    newCart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem)
+        }
+    })
+
+    cart = newCart
+  }
+
+  export {addToCart, cart, removeFromCart}
