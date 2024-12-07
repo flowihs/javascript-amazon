0628e4f1ab4b4d8613cbddf2c1e0b6c7a0cf2b1b
flowihs <zmulenko500@gmail.com>
2024-12-07 19:51:18 +0300

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,24 @@
-export const cart = [
+const cart = []
 
-]
+function addToCart (productId) {
+    let matchingItem
+  
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem
+      }
+    })
+  
+    if (matchingItem) {
+      matchingItem.quantity += 1
+    } 
+  
+    if (!matchingItem) {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      })
+    }
+  }
+
+  export {addToCart, cart}
